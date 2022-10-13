630cce2a527a37cab41945b629f4681d61df7bef
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2022-10-13 22:42:44 +0000

    add script for printing with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main() is the entry point into the program
+*
+*program returns 0 when sycessful
+*/
+int main(void)
+{
+	/*printf displays characters on the screen*/
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
