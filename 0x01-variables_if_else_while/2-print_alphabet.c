9e37360a2552cd81776f3bac9ab88a52fd30592b
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2022-10-14 22:25:59 +0000

    sdd new file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - entry point of program
+ *
+ *Return: return 0 when the program is successful
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	putchar(letter);
+	}
+
+	return (0);
+}
