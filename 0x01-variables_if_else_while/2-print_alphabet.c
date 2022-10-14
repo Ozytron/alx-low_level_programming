5935975f45c27118e8c648075816caa7f265c85d
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2022-10-14 22:41:32 +0000

    add new file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  *main - entry point of program
  *
@@ -6,12 +8,12 @@
  */
 int main(void)
 {
-	int letter;
+	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-	putchar(letter);
-	putchar('\n');
+		putchar(letter);
+		putchar('\n');
 	}
 
 	return (0);
