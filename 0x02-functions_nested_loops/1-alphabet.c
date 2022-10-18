d7c581c99343c91dc3a5975548adb0597bfa461c
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2022-10-18 22:53:35 +0000

    add more correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 /**
   *print_alphabet - this prints lowercase alphabets on screen
   *
@@ -10,10 +11,10 @@ void print_alphabet(void)
 {
 	int count;
 
-	for (count == 'a', count <= 'z', count++)
+	for (count = 'a'; count <= 'z'; count++)
 	{
 		_putchar(count);
 	}
 	_putchar('\n');
-	return (0);
+	return;
 }
