0226972d840b5ba268ea37e449ea69c604c978c3
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2022-10-18 20:10:42 +0000

    nested loop tasks

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *main - the program entry point, program prints _putchar on the screen
+  *
+  *Return: returns 0 when program is successful
+  */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  *print_alphabet - this prints lowercase alphabets on screen
+  *
+  *Return: zero is returned on success
+  */
+
+void print_alphabet(void)
+{
+	int count;
+
+	for (count == 'a', count <= 'z', count++)
+	{
+		putchar(count);
+	}
+	putchar('\n');
+	return (0);
+}
