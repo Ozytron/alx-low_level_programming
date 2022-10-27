f057a7a4e01027e203c9bbff950b91e44393c1e4
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2022-10-27 22:34:55 +0000

    correct 104-fibonacci.c file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,37 +1,50 @@
 #include <stdio.h>
-#define UPPER_LIMIT 1000000000
 
 /**
-  * main - entry point into program, prints the first 98 fibonacci numbers
-  *
-  * Return: returns 0 when successful
-  */
+ * main - Prints the first 98 Fibonacci numbers, starting with
+ *        1 and 2, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	unsigned long int i = 0, a = 1, j = 0, b = 2;
-	unsigned long int cont1, cont2, cont3;
-	int n;
+	int i;
+	unsigned long a = 0, b = 1, sum;
+	unsigned long a_half1, a_half2, b_half1, b_half2;
+	unsigned long half1, half2;
 
-	printf("%lu, %lu, ", a, b);
-	for (n = 2; n < 98; n++)
+	for (i = 0; i < 92; i++)
 	{
-		if (a + b > UPPER_LIMIT || j > 0 || i > 0)
+		sum = a + b;
+		printf("%lu, ", sum);
+
+		a = b;
+		b = sum;
+	}
+
+	a_half1 = a / 10000000000;
+	b_half1 = b / 10000000000;
+	a_half2 = a % 10000000000;
+	b_half2 = b % 10000000000;
+
+	for (i = 93; i < 99; i++)
+	{
+		half1 = a_half1 + b_half1;
+		half2 = a_half2 + b_half2;
+		if (a_half2 + b_half2 > 9999999999)
 		{
-			cont1 = (a + b) / UPPER_LIMIT;
-			cont2 = (a + b) % UPPER_LIMIT;
-			cont3 = i + j + cont1;
-			i = j, j = cont3;
-			a = b, b = cont2;
-			printf("%lu%010lu", j, b);
+			half1 += 1;
+			half2 %= 10000000000;
 		}
-		else
-		{
-			cont2 = a + b;
-			a = b, b = cont2;
-			printf("%lu", b);
-		}
-		if (n != 97)
+
+		printf("%lu%lu", half1, half2);
+		if (i != 98)
 			printf(", ");
+
+		a_half1 = b_half1;
+		a_half2 = b_half2;
+		b_half1 = half1;
+		b_half2 = half2;
 	}
 	printf("\n");
 	return (0);
