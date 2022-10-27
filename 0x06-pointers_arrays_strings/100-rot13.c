2e5f18ce9ad179e4431621c6bd09dd7eb4ff7a7d
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2022-10-27 14:59:58 +0000

    add advanced task

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - encodes strings using ROT-13
+ *@c: string
+ * Return: Modified string
+ */
+
+char *rot13(char *c)
+{
+	int i, j;
+	char a[52] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	char s[52] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+
+	i = 0;
+
+	while (c[i])
+	{
+		j = 0;
+
+		while (j < 52)
+		{
+			if (c[i] == a[j])
+			{
+				c[i] = s[j];
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (c);
+}
