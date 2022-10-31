b1046b2708d27c0a61a737ccc5fa7bd1e961f0d3
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2022-10-31 22:54:55 +0000

    add correction

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -11,8 +10,6 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i;
-	if (*needle == 0)
-		return (haystack);
 
 	while (*haystack)
 	{
@@ -21,12 +18,13 @@ char *_strstr(char *haystack, char *needle)
 		if (haystack[i] == needle[i])
 		{
 
-			while (haystack[i] == needle[i])
-			{
+			do {
 				if (needle[i + 1] == '\0')
 					return (haystack);
+
 				i++;
-			}
+
+			} while (haystack[i] == needle[i]);
 		}
 
 		haystack++;
