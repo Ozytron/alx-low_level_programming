de0629efc4fb12ff069d8f9172b56ed3322a62d7
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2022-10-31 18:44:46 +0000

    correction

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,11 +12,13 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
+	char *to = dest;
+	char *from = src;
 
 	for (i = 0; i < n; i++)
 	{
-		*dest = *(src + i);
-		dest++;
+		*to = *(from + i);
+		to++;
 	}
-	return (dest);
+	return (to);
 }
