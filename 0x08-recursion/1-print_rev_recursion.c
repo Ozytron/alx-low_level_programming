85a33ca9cf0fac2a6618de2fec6446153d8a0add
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2022-11-02 17:29:14 +0000

    correct code to use recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,15 +8,13 @@
 
 void _puts_recursion(char *s)
 {
+	int i = 0;
 
-	if (*s == '\0')
+	while (s[i] != '\0')
 	{
-		_putchar('\n');
-		return;
+		_putchar(s[i]);
+		i++;
 	}
 
-		_putchar(*s);
-		s++;
-
-		_puts_recursion(s);
+	_putchar('\n');
 }
