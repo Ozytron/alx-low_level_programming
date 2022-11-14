ab86b74b580c301c30c26b1a9e77393f1e1946d3
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2022-11-14 14:49:48 +0000

    add files

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef OBJECT_LIKE_MICRO_H
+#define OBJECT_LIKE_MICRO_H
+/**
+ *Description: a header file that defines a macro named
+ *             SIZE as an abbreviation for the token 1024.
+ */
+
+#define  SIZE 1024
+
+#endif /* OBJECT_LIKE_MICRO_H */
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_LIKE_MICRO_H
+#define FUNCTION_LIKE_MICRO_H
+
+/**
+ * Description: a function-like macro ABS(x) that
+ *              computes the absolute value of a number x.
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif /* FUNCTION_LIKE_MICRO_H */
