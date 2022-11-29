d9758fb1d74ad52cce2e767b0b316cd18429df1b
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2022-11-29 15:45:08 +0000

    add func that calculates number of nodes in a list and clean repo

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: The singly linked list.
+ * Return: he number of nodes.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t node_number = 0;
+
+	while (h != NULL)
+	{
+		node_number++;
+		h = h->next;
+	}
+	return (node_number);
+}
