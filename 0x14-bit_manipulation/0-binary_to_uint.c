d7c0031100f2098be794ba56de4b42eebd17f537
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2022-12-01 16:38:42 +0000

    correct documentation style in 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
- * @b: pointer to a string of 0 and 1 chars. 
+ * @b: pointer to a string of 0 and 1 chars.
  * Return: the converted number, or 0 if b is NULL
  *         or here is one or more chars in the
  *          string b that is not 0 or 1
